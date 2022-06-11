73f71e5d4d806b7a3ae839205a0ee6272ade9dd2
Ryan S <ryansateles@gmail.com>
2022-06-10 23:41:28 -0300

    otherVersionFeat(): only sum everything with cases

--- main.hs
+++ main.hs
@@ -4,10 +4,15 @@ main = do
  --transforma a linha numa lista
  frames <- return (map digitToInt $ filter (/=' ') line)
  
- print $ getBowlingScore frames 0 0
+ print $ getBowlingScore frames
 
-getBowlingScore :: [Int] -> Int -> Int -> Int
-getBowlingScore [] strike spare = 0
-getBowlingScore (h:t) strike spare = h + (getBowlingScore t 0 0)
+getBowlingScore :: [Int] -> Int 
+getBowlingScore ([]) = 0
+getBowlingScore (h:[]) = h
+getBowlingScore (h:b:[]) = h + b
+getBowlingScore (h:b:l:[]) = h + b + l
+getBowlingScore (h:b:l:t) = if (h == 10)                then h + b + l + getBowlingScore(b:l:t)
+                            else if (((h + b) == 10))   then h + b + l + getBowlingScore(l:t)
+                            else h + b + getBowlingScore(l:t) 
 
 
