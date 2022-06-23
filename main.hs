fffd8b0fb817371d6c4e00d700102f9348db87a9
Ryan S <ryansateles@gmail.com>
2022-06-23 19:47:19 -0300

    refacor: remove some unused functions

--- main.hs
+++ main.hs
@@ -2,13 +2,14 @@ import Data.Char
 import Bowling
 
 main = do
- -- line <- getLine
- --transforma a linha numa lista
- -- frames <- return (map read $ words line :: [Int])
+ --line <- getLine
+ --frames <- return (map read $ words line :: [Int])
  
- print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,6,4,8,6] --frames
+ print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,6,4,8,6]
 
- print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,10,8,6,5] --frames
+ print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,10,8,6,5] 
 
- print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] --frames
+ print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] 
+
+
 
