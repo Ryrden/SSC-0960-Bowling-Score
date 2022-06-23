7553dc6d99da780f753253f8e99cbf5184bcb5c6
Ryan S <ryansateles@gmail.com>
2022-06-23 17:26:06 -0300

    refactor: remove function getBowlingScore

--- main.hs
+++ main.hs
@@ -10,19 +10,6 @@ main = do
 
  print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] --frames
 
-getBowlingScore :: [Int] -> Int
---Não é necessário o que está comentado
---getBowlingScore ([]) = 0
---getBowlingScore (h:[]) = h
---getBowlingScore (h:b:[]) = h + b
-getBowlingScore (h:b:l:[]) = h + b + l
-getBowlingScore (h:b:l:t) = if h == 10                then h + b + l + getBowlingScore(b:l:t)
-                            else if (h + b) == 10   then h + b + l + getBowlingScore(l:t)
-                            else h + b + getBowlingScore(l:t) 
-
-
-
-
 getBowlingOutput :: Int -> [Int] -> String
 getBowlingOutput 9 (a:b:c) =  if       a    == 10 then getBowlingOutput 9 [a] ++ " "   ++ getBowlingOutput 9     (b:c) 
                               else if (a+b) == 10 then getBowlingOutput 9 [a] ++ " /"  ++ getBowlingOutput 9      c
