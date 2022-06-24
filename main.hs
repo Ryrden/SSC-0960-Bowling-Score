b7bbe8028582578027594f67811b5684d2995d0c
Ryan S <ryansateles@gmail.com>
2022-06-24 11:34:38 -0300

    test: new test cases created

--- main.hs
+++ main.hs
@@ -10,6 +10,8 @@ main = do
  print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,10,8,6,5] 
 
  print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] 
-
-
+ 
+ print $ getBowlingOutput 0 [8,0,7,0,5,3,9,1,9,1,10,8,0,5,1,3,7,9,0]
+ 
+ print $ getBowlingOutput 0 [8,2,9,0,4,4,7,2,9,0,10,10,8,0,3,5,9,1,7]
 
