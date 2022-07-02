b9e4f14646a26b3a94db66c5a1be8df738eb74f9
Ryan <ryansateles@gmail.com>
2022-07-02 16:35:01 -0300

    feature: old function to get score it's back

--- main.hs
+++ main.hs
@@ -5,9 +5,9 @@ main = do
  --line <- getLine
  --frames <- return (map read $ words line :: [Int])
  
- print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,6,4,8,6]
+ print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,6,4,8,6]
 
- print $ getBowlingOutput 0 [1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6,4,10,8,6,5] 
+ print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,6,5] 
 
  print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] 
  
