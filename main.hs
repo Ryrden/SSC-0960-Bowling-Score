e83e52a44533d4e702f97c3a7da0107fb8d3aaed
Ryan <ryansateles@gmail.com>
2022-07-02 15:39:56 -0300

    refactor: print ajust

--- main.hs
+++ main.hs
@@ -5,11 +5,11 @@ main = do
  --line <- getLine
  --frames <- return (map read $ words line :: [Int])
  
- print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,6,4,8,6]
-
- print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,6,5] 
-
- print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10] 
+ print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]
+ 
+ print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,6,5]
+ 
+ print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10]
  
  print $ getBowlingOutput 0 [8,0,7,0,5,3,9,1,9,1,10,8,0,5,1,3,7,9,0]
  
