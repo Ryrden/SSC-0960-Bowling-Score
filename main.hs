0d490a3a27e9f0dae4054e6054099d47a2140699
Ryan <ryansateles@gmail.com>
2022-07-03 01:03:00 -0300

    fix: created and checked some bug testCases

--- main.hs
+++ main.hs
@@ -9,6 +9,14 @@ main = do
  
  print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,10,10,10]
  
+ print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,5,2]
+ 
+ print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,10,5,2]
+
+ print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,10,5,5]
+
+ print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,6,4,6]
+ 
  print $ getTableScore 0 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,2,5]
  
  print $ getTableScore 0 0 [8,0,7,0,5,3,9,1,9,1,10,8,0,5,1,3,7,9,0]
