ef163e01ecd0f58a1ed3b3492e2a31372f6d798f
Ryan <ryansateles@gmail.com>
2022-07-03 00:01:49 -0300

    feature: function getTableScore created

--- Bowling.hs
+++ Bowling.hs
@@ -1,22 +1,24 @@
 module Bowling where 
 
-getBowlingScore :: [Int] -> Int
---Não é necessário o que está comentado
-getBowlingScore ([]) = 0
-getBowlingScore (h:[]) = h
-getBowlingScore (h:b:[]) = h + b
-getBowlingScore (h:b:l:[]) = h + b + l
-getBowlingScore (h:b:l:t) = if h == 10              then h + b + l + getBowlingScore(b:l:t)
-                            else if (h + b) == 10   then h + b + l + getBowlingScore(l:t)
-                            else h + b + getBowlingScore(l:t) 
+getTableScore :: Int -> Int -> [Int] -> String
+getTableScore score 9 (a:b:c:[]) =  if    (a+b+c) == 30 then getTableScore score 9 [a] ++   " "   ++ getTableScore  score          9    [b]     ++   " "  ++ getTableScore (score+a+b+c) 10   [c]
+                                    else if (a+b) == 10 then getTableScore score 9 [a] ++  " / "  ++ getTableScore (score+a+b+c)   10   [c]
+                                    else                     getTableScore score 9 [a] ++   " "   ++ getTableScore  score          9    [b]     ++   " "  ++ getTableScore (score+a+b)   10   [c]
+getTableScore score 9 (a:b:c)    =                           getTableScore score 9 [a] ++   " "   ++ getTableScore (score+a+b)     10   [b]
+getTableScore score a (b:c:d:e)  =  if       b    == 10 then getTableScore score a [b] ++ " _ | " ++ getTableScore (score+b+c+d)  (a+1) (c:d:e)
+                                    else if (b+c) == 10 then getTableScore score a [b] ++ " / | " ++ getTableScore (score+b+c+d)  (a+1) (d:e) 
+                                    else                     getTableScore score a [b] ++   " "   ++ getTableScore  score           a   [c]      ++ " | " ++ getTableScore (score+b+c)  (a+1) (d:e)
+getTableScore score 10 [10] = "X" ++ " | " ++ show(score)
+getTableScore score 10 [b] = show(b) ++ " | " ++ show(score)
+getTableScore score a [10] = "X"
+getTableScore score a [b]  = show b
 
-getBowlingOutput :: Int -> [Int] -> String
-getBowlingOutput 9 (a:b:c) =  if       a    == 10 then getBowlingOutput 9 [a] ++ " "   ++ getBowlingOutput 9     (b:c) 
-                              else if (a+b) == 10 then getBowlingOutput 9 [a] ++ " / "  ++ getBowlingOutput 9      c
-                              else                     getBowlingOutput 9 [a] ++ " "   ++ getBowlingOutput 9     (b:c)
-getBowlingOutput a (b:c:d) =  if       b    == 10 then getBowlingOutput a [b] ++ " _ | " ++ getBowlingOutput (a+1) (c:d)
-                              else if (b+c) == 10 then getBowlingOutput a [b] ++ " / | " ++ getBowlingOutput (a+1)  d 
-                              else                     getBowlingOutput a [b] ++ " "   ++ getBowlingOutput a     [c]   ++ " | " ++ getBowlingOutput (a+1) d
-getBowlingOutput a [10] = "X"
-getBowlingOutput a [b]  = show b
-getBowlingOutput _  _   = ""
+getBowlingOutput :: Int -> Int -> [Int] -> String 
+getBowlingOutput sum 9 (a:b:c)   =  if       a    == 10 then getBowlingOutput sum 9 [a] ++ " "   ++   getBowlingOutput sum 9     (b:c)
+                                    else if (a+b) == 10 then getBowlingOutput sum 9 [a] ++ " / "  ++  getBowlingOutput sum 9      c   
+                                    else                     getBowlingOutput sum 9 [a] ++ " "   ++   getBowlingOutput sum 9     (b:c)
+getBowlingOutput sum a (b:c:d)   =  if       b    == 10 then getBowlingOutput sum a [b] ++ " _ | " ++ getBowlingOutput sum (a+1) (c:d)
+                                    else if (b+c) == 10 then getBowlingOutput sum a [b] ++ " / | " ++ getBowlingOutput sum (a+1)  d 
+                                    else                     getBowlingOutput sum a [b] ++ " "   ++   getBowlingOutput sum a     [c]   ++ " | " ++ getBowlingOutput sum (a+1) d
+getBowlingOutput sum a [10] = "X"
+getBowlingOutput sum a [b]  = show b
--- main.hs
+++ main.hs
@@ -5,13 +5,12 @@ main = do
  --line <- getLine
  --frames <- return (map read $ words line :: [Int])
  
- print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]
+ print $ getTableScore 0 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]
  
- print $ getBowlingOutput 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,6,5]
+ print $ getTableScore 0 0 [10,10,10,10,10,10,10,10,10,10,10,10]
  
- print $ getBowlingOutput 0 [10,10,10,10,10,10,10,10,10,10,10,10]
+ print $ getTableScore 0 0 [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,8,2,5]
  
- print $ getBowlingOutput 0 [8,0,7,0,5,3,9,1,9,1,10,8,0,5,1,3,7,9,0]
+ print $ getTableScore 0 0 [8,0,7,0,5,3,9,1,9,1,10,8,0,5,1,3,7,9,0]
  
- print $ getBowlingOutput 0 [8,2,9,0,4,4,7,2,9,0,10,10,8,0,3,5,9,1,7]
-
+ print $ getTableScore 0 0 [8,2,9,0,4,4,7,2,9,0,10,10,8,0,3,5,9,1,7]
